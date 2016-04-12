8f305a821d72c01d6baa36ca8944e273985315ff
Jonas Schievink <jonas@schievink.net>
2016-04-13 00:49:09 +0200

    DummyRenderer: Save last frame of output

--- src/breeze_frontends/frontend/dummy.rs
+++ src/breeze_frontends/frontend/dummy.rs
@@ -2,12 +2,26 @@
 
 use frontend_api::{FrontendAction, Renderer, AudioSink};
 
-/// Renderer that just does nothing.
+/// Renderer that just does nothing, apart from saving the PPU output for later use. This allows
+/// users to extract single rendered frames without having to implement `Renderer`.
 #[derive(Default)]
-pub struct DummyRenderer;
+pub struct DummyRenderer {
+    last_frame: Vec<u8>,
+}
+
+impl DummyRenderer {
+    pub fn last_frame(&self) -> &[u8] {
+        &self.last_frame
+    }
+}
 
 impl Renderer for DummyRenderer {
-    fn render(&mut self, _frame_data: &[u8]) -> Option<FrontendAction> { None }
+    fn render(&mut self, frame_data: &[u8]) -> Option<FrontendAction> {
+        self.last_frame.clear();
+        self.last_frame.extend_from_slice(frame_data);
+        None
+    }
+
     fn set_rom_title(&mut self, _title: &str) {}
 }
 
